dc4aa189b0bde44916612c67dd430da5e084cfdb
Christian Luke <13chrisl@gmail.com>
2026-01-01 15:17:38 -0700

    Created function to parse sender data from messages

--- src/quickstart.py
+++ src/quickstart.py
@@ -39,6 +39,8 @@ def main():
     results = service.users().labels().list(userId="me").execute()
     labels = results.get("labels", [])
 
+
+
     if not labels:
       print("No labels found.")
       return
